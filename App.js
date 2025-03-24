7943a7c4aa91fab7f526078d828a153dd0d302d5
Sunil Dhakar <sunildhakar078@gmail.com>
2025-03-24 16:01:17 +0530

    Component in Loop with FlatList

--- App.js
+++ App.js
@@ -7,42 +7,61 @@
 
 import React, { useState } from 'react';
 import {
+  FlatList,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
 const App = () => {
-  const users = [{ id: 1, name: "Sunil" }, { id: 2, name: "Rahul" }, { id: 3, name: "Amit" }, { id: 4, name: "bunty" }, { id: 6, name: "tony" }, { id: 7, name: "pay" }, { id: 8, name: "money" }, { id: 9, name: "test" },];
+  const users = [{ id: 1, name: "Sunil", email: "sunil@gmail.com" }, { id: 2, name: "Rahul", email: "rahul@gmail.com" }, { id: 3, name: "Amit", email: "amit@gmail.com" }, { id: 4, name: "bunty", email: "bunty@gmail.com" }, { id: 6, name: "tony", email: "tony@gmail.com" }, { id: 7, name: "pay", email: "pay@gmail.com" }, { id: 8, name: "money", email: "money@gmail.com" }, { id: 9, name: "test", email: "test@gmail.com" },];
 
 
 
   return (
     <View>
-      <Text style={{ fontSize: 30 }}> Grid with Dynamic Data</Text>
+      <Text style={{ fontSize: 25 }}> Component in Loop with FlatList</Text>
 
-      <View style={{flex:1,flexDirection:'row',flexWrap:'wrap'}}> 
-      {
-      //TODO static data
-      /* <Text style={style.item}> Sunil</Text>
-      <Text style={style.item}> Sunil</Text>
-      <Text style={style.item}> Sunil</Text>
-      <Text style={style.item}> Sunil</Text> */
-      
-      users.map((item)=><Text style={style.item}>{item.name}</Text>)
-      
-      }
-
-      </View>
+      <FlatList data={users} renderItem={({ item }) =><UserData item={item}/>}>
 
 
+      </FlatList>
+
+      //component-with-list
     </View>
   );
+}
+const UserData=(props)=>{
+const item=props.item;
+  return (
+<View style={style.box}>
+        <Text style={style.item}>{item.name}</Text>
+        <Text style={style.item}>{item.email}</Text>
+
+      </View>
+  )
 }
 
 const style = StyleSheet.create({
   item:
-    { fontSize: 24, color: '#fff', padding: 10, borderColor: 'black', backgroundColor: 'blue', margin: 10,width:120,height:120,textAlignVertical:'center',textAlign:'center' }
+  {
+    fontSize: 24,
+    color: 'orange',
+    padding: 10,
+    margin: 2,
+    flex: 1,
+    backgroundColor:"blue",
+    textAlign:'center',
+    
+  },
+
+  box: {
+    flexDirection: 'row',
+    borderColor: 'orange',
+    borderWidth: 2,
+    marginBottom: 10
+
+  }
 
 })
 
