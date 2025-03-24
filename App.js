c3bb5a67ea96ea1e2ebf6b0f6af2e01577d8ac4a
Sunil Dhakar <sunildhakar078@gmail.com>
2025-03-24 15:37:36 +0530

    grid with dynamic and statics

--- App.js
+++ App.js
@@ -7,25 +7,34 @@
 
 import React, { useState } from 'react';
 import {
-  ScrollView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
 const App = () => {
-  const users = [{ id: 1, name: "Sunil" }, { id: 2, name: "Rahul" }, { id: 3, name: "Amit" }, { id: 4, name: "bunty" }, { id: 6, name: "tony" }, { id: 7, name: "pay" }, { id: 8, name: "money" }, { id: 9, name: "test" },{ id: 1, name: "Sunil" }, { id: 2, name: "Rahul" }, { id: 3, name: "Amit" }, { id: 4, name: "bunty" }, { id: 6, name: "tony" }, { id: 7, name: "pay" }, { id: 8, name: "money" }, { id: 9, name: "test" }];
+  const users = [{ id: 1, name: "Sunil" }, { id: 2, name: "Rahul" }, { id: 3, name: "Amit" }, { id: 4, name: "bunty" }, { id: 6, name: "tony" }, { id: 7, name: "pay" }, { id: 8, name: "money" }, { id: 9, name: "test" },];
 
 
 
   return (
     <View>
-      <Text style={{ fontSize: 30 }}> List with map function</Text>
-      <ScrollView style={{marginBottom:50}}>{
-        users.map((item) => <Text style={style.item}> {item.name}</Text>)
+      <Text style={{ fontSize: 30 }}> Grid with Dynamic Data</Text>
 
+      <View style={{flex:1,flexDirection:'row',flexWrap:'wrap'}}> 
+      {
+      //TODO static data
+      /* <Text style={style.item}> Sunil</Text>
+      <Text style={style.item}> Sunil</Text>
+      <Text style={style.item}> Sunil</Text>
+      <Text style={style.item}> Sunil</Text> */
+      
+      users.map((item)=><Text style={style.item}>{item.name}</Text>)
+      
       }
-      </ScrollView>
+
+      </View>
+
 
     </View>
   );
@@ -33,7 +42,7 @@ const App = () => {
 
 const style = StyleSheet.create({
   item:
-    { fontSize: 24, color: '#fff', padding: 10, borderColor: 'black', backgroundColor: 'blue', margin: 10 }
+    { fontSize: 24, color: '#fff', padding: 10, borderColor: 'black', backgroundColor: 'blue', margin: 10,width:120,height:120,textAlignVertical:'center',textAlign:'center' }
 
 })
 
